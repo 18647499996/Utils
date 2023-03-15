1403ef72da0405dd74e76e55ac3f3f39b8218b01
刘冬涵 <309147563@qq.com>
2023-03-15 11:15:44 +0800

    添加手机号码脱敏验证

--- app/src/main/java/com/liudonghan/main/MainActivity.java
+++ app/src/main/java/com/liudonghan/main/MainActivity.java
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.heytap.msp.push.HeytapPushManager;
 import com.liudonghan.utils.ADPicturePhotoUtils;
+import com.liudonghan.utils.ADRegexUtils;
 import com.liudonghan.utils.ADScreenRecordUtils;
 import com.liudonghan.utils.ADTextStyleUtils;
 
@@ -24,35 +25,28 @@ public class MainActivity extends AppCompatActivity implements ADPicturePhotoUti
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.activity_main_tv);
-        textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ADTextStyleUtils.getInstance()
-                        // 上下文
-                        .from(MainActivity.this)
-                        // 构建view
-                        .findView(textView)
-                        // 原始文本属性
-                        .originAttr(22, R.color.colorAccent)
-                        // 改变文本属性
-                        .changeAttr(14, R.color.colorPrimary)
-                        // 改变位置
-                        .changePosition(0, 1)
-                        // 设置文本图标属性
-                        .drawablesIconAttr(R.mipmap.ic_launcher)
-                        // 设置图标大小
-                        .drawablesSize(textView.getHeight(), textView.getHeight())
-                        // 设置文本
-                        .textDescription("￥18990.00")
-                        // 构建
-                        .builder();
-                ADScreenRecordUtils.getInstance().register(MainActivity.this, new ADScreenRecordUtils.OnContentObserverUtilsListener() {
-                    @Override
-                    public void isScreenRecord(boolean isScreenRecord) {
-                        Log.d("录屏监听：",String.valueOf(isScreenRecord));
-                    }
-                });
-            }
+        Log.d("MAC_LIU", "验证字符串：" + ADRegexUtils.getInstance().getMobileAcute("哈哈18647499996"));
+        textView.setOnClickListener(view -> {
+            ADTextStyleUtils.getInstance()
+                    // 上下文
+                    .from(MainActivity.this)
+                    // 构建view
+                    .findView(textView)
+                    // 原始文本属性
+                    .originAttr(22, R.color.colorAccent)
+                    // 改变文本属性
+                    .changeAttr(14, R.color.colorPrimary)
+                    // 改变位置
+                    .changePosition(0, 1)
+                    // 设置文本图标属性
+                    .drawablesIconAttr(R.mipmap.ic_launcher)
+                    // 设置图标大小
+                    .drawablesSize(textView.getHeight(), textView.getHeight())
+                    // 设置文本
+                    .textDescription("￥18990.00")
+                    // 构建
+                    .builder();
+            ADScreenRecordUtils.getInstance().register(MainActivity.this, isScreenRecord -> Log.d("录屏监听：", String.valueOf(isScreenRecord)));
         });
         ADPicturePhotoUtils.getInstance().init(this).onCallBack(this);
     }
--- utils/src/main/java/com/liudonghan/utils/ADRegexUtils.java
+++ utils/src/main/java/com/liudonghan/utils/ADRegexUtils.java
@@ -1,5 +1,8 @@
 package com.liudonghan.utils;
 
+import android.text.TextUtils;
+import android.util.Log;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,9 +14,65 @@ import java.util.regex.Pattern;
  */
 public class ADRegexUtils {
 
+    private static final String MAC_LIU = "Mac_Liu";
     private static volatile ADRegexUtils instance = null;
 
     private ADRegexUtils() {
+    }
+
+    /**
+     * 获取消息包含手机号码脱敏显示
+     *
+     * @param content 内容
+     * @return String
+     */
+    public String getMobileAcute(String content) {
+        if (TextUtils.isEmpty(content)) {
+            return "";
+        }
+        if (content.length() < 11) {
+            return content;
+        }
+        // 将字符串切割成数组
+        String[] split = content.split("");
+        int start = -1;
+        int end = -1;
+        int count = 0;
+        for (int i = 0; i < split.length; i++) {
+            if (isNumber(split[i])) {
+                // 当前字符包含数字
+                if (count < 11) {
+                    if (-1 == start) {
+                        start = i;
+                    } else {
+                        end = i;
+                    }
+                    count++;
+                }
+            } else {
+                // 不包含数字
+                if (count != 11) {
+//                    Log.d(MAC_LIU, "Count值：" + count);
+                    start = -1;
+                    end = -1;
+                    count = 0;
+                }
+            }
+        }
+//        Log.d(MAC_LIU, "Count总数：" + count);
+//        Log.d(MAC_LIU, "开始：" + start + " ----- " + "结束：" + end);
+//        Log.d(MAC_LIU, "截取字符串：" + content.substring(start, end + 1));
+        if (isTelPhoneNumber(content.substring(start, end + 1))) {
+            String before = content.substring(0,start);
+            String phone = content.substring(start,end + 1);
+            String suffix = content.substring(end + 1);
+            Log.d(MAC_LIU, "前缀：" + before);
+            Log.d(MAC_LIU, "手机号码：" + mobileNaked(phone));
+            Log.d(MAC_LIU, "后缀：" + suffix);
+            return before + mobileNaked(phone) + suffix;
+        } else {
+            return content;
+        }
     }
 
     public static ADRegexUtils getInstance() {
@@ -30,16 +89,54 @@ public class ADRegexUtils {
     }
 
     /**
+     * 手机号码脱敏显示
+     * @param num 脱敏
+     * @return String
+     */
+    public static String mobileNaked(String num) {
+        if (TextUtils.isEmpty(num)) {
+            return "";
+        }
+        if (!isTelPhoneNumber(num)){
+            return num;
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(num.subSequence(0, 3));
+        for (int i = 0; i < num.length() - 5; i++) {
+            sb.append("*");
+        }
+        sb.append(num.substring(num.length() - 2));
+        return sb.toString();
+    }
+
+
+    /**
      * 验证数字
      *
      * @param str 字符串
      * @return boolean
      */
-    public boolean IsNumber(String str) {
+    public boolean isNumber(String str) {
         String regex = "^[0-9]*$";
         return match(regex, str);
     }
 
+    public static boolean isTelPhoneNumber(String value) {
+        if (value != null && value.length() == 11) {
+            Pattern pattern = Pattern.compile("^1[3|4|5|6|7|8][0-9]\\d{8}$");
+            Matcher matcher = pattern.matcher(value);
+            return matcher.matches();
+        }
+        return false;
+    }
+
+    /**
+     * 正则过滤器
+     *
+     * @param regex 匹配符
+     * @param str   字符串
+     * @return boolean
+     */
     private static boolean match(String regex, String str) {
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(str);
