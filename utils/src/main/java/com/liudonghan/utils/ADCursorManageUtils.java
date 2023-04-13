bcf6c27dc55a3355f41eb5a194926b5d84f0175d
刘冬涵 <309147563@qq.com>
2023-04-13 12:15:09 +0800

    修复图片与视频列表文件总数问题

--- utils/src/main/java/com/liudonghan/utils/ADCursorManageUtils.java
+++ utils/src/main/java/com/liudonghan/utils/ADCursorManageUtils.java
@@ -441,6 +441,7 @@ public class ADCursorManageUtils {
         if (isContains) {
             Log.i(TAG, "已添加：" + dir);
             dirs.get(position).getMediaPath().add(mediaModel);
+            dirs.get(position).setFileCount(dirs.get(position).getMediaPath().size());
         } else {
             Log.i(TAG, "未添加：" + dir);
             ImageFolderModel imageFolderModel = new ImageFolderModel();
