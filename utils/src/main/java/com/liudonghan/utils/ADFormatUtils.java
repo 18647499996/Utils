7f19d465733c21e53ca7f85ed4b20bedab074e0d
刘冬涵 <309147563@qq.com>
2023-10-08 15:48:57 +0800

    添加日期计算差

--- utils/src/main/java/com/liudonghan/utils/ADFormatUtils.java
+++ utils/src/main/java/com/liudonghan/utils/ADFormatUtils.java
@@ -161,4 +161,46 @@ public class ADFormatUtils {
         return 0;
     }
 
+    /**
+     * todo 根据date日期获取时间差
+     * 例如：几年前、几个月前、几天前、几小时前、几分钟前、刚刚
+     *
+     * @param date 日期
+     * @return String
+     */
+    public String getTimeFormatText(Date date) {
+        long minute = 60 * 1000;// 1分钟
+        long hour = 60 * minute;// 1小时
+        long day = 24 * hour;// 1天
+        long month = 31 * day;// 月
+        long year = 12 * month;// 年
+
+        if (date == null) {
+            return null;
+        }
+        long diff = new Date().getTime() - date.getTime();
+        long r;
+        if (diff > year) {
+            r = (diff / year);
+            return r + "年前";
+        }
+        if (diff > month) {
+            r = (diff / month);
+            return r + "个月前";
+        }
+        if (diff > day) {
+            r = (diff / day);
+            return r + "天前";
+        }
+        if (diff > hour) {
+            r = (diff / hour);
+            return r + "小时前";
+        }
+        if (diff > minute) {
+            r = (diff / minute);
+            return r + "分钟前";
+        }
+        return "刚刚";
+    }
+
 }
