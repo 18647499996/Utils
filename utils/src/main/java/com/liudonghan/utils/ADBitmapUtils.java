a88859335553127d4c0822ee09e439078be0a72f
刘冬涵 <309147563@qq.com>
2025-12-12 11:31:36 +0800

    修复部分代码

--- utils/src/main/java/com/liudonghan/utils/ADBitmapUtils.java
+++ utils/src/main/java/com/liudonghan/utils/ADBitmapUtils.java
@@ -3,8 +3,10 @@ package com.liudonghan.utils;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.MediaMetadataRetriever;
+import android.util.Base64;
 
 import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
@@ -51,6 +53,39 @@ public class ADBitmapUtils {
         opts.inJustDecodeBounds = false;
         //为true时，返回的bitmap为null
         return BitmapFactory.decodeByteArray(decodes, 0, decodes.length, opts);
+    }
+
+    /**
+     * bitmap转为base64
+     * @param bitmap bitmap图片引用
+     * @return String
+     */
+    public String bitmapToBase64(Bitmap bitmap) {
+        String result = null;
+        ByteArrayOutputStream baos = null;
+        try {
+            if (bitmap != null) {
+                baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+                baos.flush();
+                baos.close();
+
+                byte[] bitmapBytes = baos.toByteArray();
+                result = Base64.encodeToString(bitmapBytes, Base64.NO_WRAP);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (baos != null) {
+                    baos.flush();
+                    baos.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
     }
 
     /**
